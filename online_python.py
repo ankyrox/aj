01caed5af615c781dac1853cd6fe42e466756bdf
jb <37432142+ankyrox@users.noreply.github.com>
2018-08-23 11:43:29 +0530

    Create online_python.py

---
+++ online_python.py
@@ -0,0 +1,15 @@
+#HappyBirthday
+
+def happyBirthday(person):
+    print("Happy BIrtHday to you!".title())
+    print("Happy BIrtHday to you!".title())
+    print("Happy BIrtHday, dear " + person + ".".title())
+    print("Happy BIrtHday to you!".title())
+
+def main():
+    userName = input("Enter the name of Birthday person:".title())
+    happyBirthday(userName)
+
+main()
+    
+    
